894e570b61757fbf90804074c24236af30e91baa
rueegg.michael@gmail.com <rueegg.michael@gmail.com@d3a0eb50-8b9f-4acb-0a96-f3fd8a11e064>
2011-03-23 12:17:22 +0000

    populating location list with DB entries

--- src/com/traveldiary/controller/TravelDiary.java
+++ src/com/traveldiary/controller/TravelDiary.java
@@ -2,6 +2,7 @@ package com.traveldiary.controller;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -9,8 +10,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.traveldiary.pd.Location;
+import com.traveldiary.persistence.Database;
 
 public class TravelDiary extends ListActivity {
 	private static final int ACTIVITY_CREATE = 0;
@@ -18,6 +22,22 @@ public class TravelDiary extends ListActivity {
 
 	private static final int TAKE_LOCATION_ID = Menu.FIRST;
 	private static final int DELETE_LOCATION_ID = Menu.FIRST + 1;
+
+	private Database mDatabase;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.locations_list);
+		mDatabase = new Database(this);
+		mDatabase.open();
+		fillData();
+
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+		registerForContextMenu(lv);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,34 +52,33 @@ public class TravelDiary extends ListActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-        switch(item.getItemId()) {
-            case TAKE_LOCATION_ID:
-                takeLocation();
-                return true;
-        }
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+		case TAKE_LOCATION_ID:
+			takeLocation();
+			return true;
+		}
 
-        return super.onMenuItemSelected(featureId, item);
-    }
+		return super.onMenuItemSelected(featureId, item);
+	}
 
 	private void takeLocation() {
 		Intent i = new Intent(this, LocationEdit.class);
 		startActivityForResult(i, ACTIVITY_CREATE);
 	}
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	private void fillData() {
+		Cursor locationCursor = mDatabase.getLocations();
+		startManagingCursor(locationCursor);
 
-		setContentView(R.layout.locations_list);
-		String[] locations = getResources().getStringArray(
-				R.array.locations_array);
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.location_item,
-				locations));
-		ListView lv = getListView();
-		lv.setTextFilterEnabled(true);
-		registerForContextMenu(lv);
+		String[] from = new String[] { Database.KEY_TITLE,
+				Database.KEY_DATETIME };
+		int[] to = new int[] { R.id.title, R.id.datetime };
+
+		SimpleCursorAdapter locations = new SimpleCursorAdapter(this,
+				R.layout.location_item, locationCursor, from, to);
+		setListAdapter(locations);
 	}
 
 	@Override
@@ -75,31 +94,25 @@ public class TravelDiary extends ListActivity {
 		case DELETE_LOCATION_ID:
 			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
 					.getMenuInfo();
-			// TODO delete location
+			mDatabase.delLocation(new Location(info.id));
+			fillData();
 			return true;
 		}
 		return super.onContextItemSelected(item);
 	}
 
 	@Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent i = new Intent(this, LocationEdit.class);
-        //TODO store information in intent to show location details
-        startActivityForResult(i, ACTIVITY_EDIT);
-    }
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Intent i = new Intent(this, LocationEdit.class);
+		// TODO store information in intent to show location details
+		startActivityForResult(i, ACTIVITY_EDIT);
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        Bundle extras = intent.getExtras();
-        switch(requestCode) {
-            case ACTIVITY_CREATE:
-            	//TODO create new location
-                break;
-            case ACTIVITY_EDIT:
-                //TODO update location with new notes and picture, then refresh
-                break;
-        }
-    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+		fillData();
+	}
 }
--- src/com/traveldiary/persistence/Database.java
+++ src/com/traveldiary/persistence/Database.java
@@ -1,110 +1,93 @@
 package com.traveldiary.persistence;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.traveldiary.pd.Location;
 
-public class Database extends SQLiteOpenHelper {
-	
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "traveldiary.db";
-    private static final String TABLE_NAME = "locations";
+public class Database {
+	public static final String KEY_TITLE = "title";
+	public static final String KEY_DATETIME = "datetime";
+	public static final String KEY_ROWID = "_id";
 
-    Database(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "traveldiary.db";
+	private static final String TABLE_NAME = "locations";
+	private static final String DATABASE_CREATE = "CREATE TABLE " + TABLE_NAME
+			+ " (" + KEY_ROWID + " INTEGER PRIMARY KEY," + KEY_TITLE
+			+ " TEXT NOT NULL," + KEY_DATETIME + " TEXT NOT NULL)";
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		
-		db.execSQL("CREATE TABLE " + TABLE_NAME + " (" +
-				   "id INTEGER PRIMARY KEY," +
-				   "title TEXT NOT NULL," +
-				   "datetime TEXT NOT NULL)");
-		
+	private final Context mCtx;
+	private DatabaseHelper mDbHelper;
+	private SQLiteDatabase mDb;
+
+	public Database(Context ctx) {
+		this.mCtx = ctx;
 	}
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-		onCreate(db);
-		
+	public Database open() throws SQLException {
+		mDbHelper = new DatabaseHelper(mCtx);
+		mDb = mDbHelper.getWritableDatabase();
+		return this;
 	}
-	
-	public List<Location> getLocations(SQLiteDatabase db) {
-		
-		List<Location> lst = new ArrayList<Location>();
-		Cursor crsr = db.query(TABLE_NAME, 
-							   new String[] {"id", "title", "datetime"}, 
-							   null, 
-							   null, 
-							   null, 
-							   null, 
-							   "datetime");
-		
-		while(crsr.moveToNext()) {
-			lst.add(new Location(crsr.getInt(0),
-								 crsr.getString(1),
-								 new Date(Date.parse(crsr.getString(2)))));
+
+	public void close() {
+		mDbHelper.close();
+	}
+
+	private static class DatabaseHelper extends SQLiteOpenHelper {
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
 		}
-		
-		return lst;
-		
-	}
-	
-	public boolean addLocation(SQLiteDatabase db, Location loc) {
-		
-		long ret = db.insert(TABLE_NAME, 
-							 null, 
-							 getContentValues(loc));
-		
-		if(ret >= 0) return true;
-		else		 return false;
-		
-	}
-	
-	public boolean modLocation(SQLiteDatabase db, Location loc) {
-		
-		long ret = db.update(TABLE_NAME, 
-							 getContentValues(loc),
-							 "id=?", 
-							 new String[] {Integer.toString(loc.getId())});
-		
-		if(ret >= 1) return true;
-		else		 return false;
-		
-	}
-	
-	public boolean delLocation(SQLiteDatabase db, Location loc) {
-		
-		int ret = db.delete(TABLE_NAME, 
-							"id=?", 
-							new String[] {Integer.toString(loc.getId())});
-		
-		if(ret >= 1) return true;
-		else         return false;
-		
-	}
-	
-	private ContentValues getContentValues(Location loc) {
-		
-		ContentValues values = new ContentValues();
-		
-		values.put("id", loc.getId());
-		values.put("title", loc.getTitle());
-		values.put("datetime", loc.getTmstmp().toLocaleString());
-		
-		return values;
-		
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(DATABASE_CREATE);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+			onCreate(db);
+		}
 	}
 
+	public Cursor getLocations() {
+		return mDb.query(TABLE_NAME, new String[] { KEY_ROWID, KEY_TITLE,
+				KEY_DATETIME }, null, null, null, null, KEY_DATETIME);
+	}
+
+	public boolean addLocation(Location loc) {
+		long ret = mDb.insert(TABLE_NAME, null, getContentValues(loc));
+
+		return ret >= 0;
+	}
+
+	public boolean modLocation(Location loc) {
+		long ret = mDb.update(TABLE_NAME, getContentValues(loc), KEY_ROWID
+				+ "=?", new String[] { Long.toString(loc.getId()) });
+
+		return ret >= 1;
+	}
+
+	public boolean delLocation(Location loc) {
+		int ret = mDb.delete(TABLE_NAME, KEY_ROWID + "=?",
+				new String[] { Long.toString(loc.getId()) });
+
+		return ret >= 1;
+	}
+
+	private ContentValues getContentValues(Location loc) {
+		ContentValues values = new ContentValues();
+
+		values.put(KEY_ROWID, loc.getId());
+		values.put(KEY_TITLE, loc.getTitle());
+		values.put(KEY_DATETIME, loc.getTmstmp().toLocaleString());
+
+		return values;
+	}
 }
