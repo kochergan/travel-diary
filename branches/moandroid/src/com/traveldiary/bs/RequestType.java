f144f3d153d1774f26cdb82bb9f88e23244226d4
rueegg.michael@gmail.com <rueegg.michael@gmail.com@d3a0eb50-8b9f-4acb-0a96-f3fd8a11e064>
2011-04-03 16:44:19 +0000

    new google maps connection and merge with Sergii's work

---
+++ branches/moandroid/src/com/traveldiary/bs/RequestType.java
@@ -0,0 +1,5 @@
+package com.traveldiary.bs;
+
+public enum RequestType {
+	EMPTY, POST, GET, DELETE
+}
--- branches/moandroid/src/com/traveldiary/pd/Location.java
+++ branches/moandroid/src/com/traveldiary/pd/Location.java
@@ -1,34 +1,36 @@
 package com.traveldiary.pd;
 
-import java.io.InputStream;
 import java.util.Date;
 
 public final class Location {
 	private final long id;
 	private final String title;
-	private final Date tmstmp;
 	private final String text;
-	private final InputStream image;
+	private final long tmstmp;
 	private final double latitude;
 	private final double longitude;
 
-	public Location(long id, String title, Date tmstmp, String text,
-			InputStream image, double latitude, double longitude) {
+	public Location(long id, String title, String text, long tmstmp,
+			double latitude, double longitude) {
 		this.id = id;
 		this.title = title;
-		this.tmstmp = tmstmp;
 		this.text = text;
-		this.image = image;
+		this.tmstmp = tmstmp;
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
 
-	public Location(long id, String title, Date tmstmp) {
-		this(id, title, tmstmp, null, null, 0.0, 0.0);
+	public Location(String title, String text, long tmstmp, double latitude,
+			double longitude) {
+		this(-1, title, text, tmstmp, latitude, longitude);
+	}
+
+	public Location(long id, String title, long tmstmp) {
+		this(id, title, "", tmstmp, 0.0, 0.0);
 	}
 
 	public Location(long id) {
-		this(id, "", new Date(), null, null, 0.0, 0.0);
+		this(id, "", "", new Date().getTime(), 0.0, 0.0);
 	}
 
 	public long getId() {
@@ -39,16 +41,12 @@ public final class Location {
 		return title;
 	}
 
-	public Date getTmstmp() {
-		return tmstmp;
-	}
-
 	public String getText() {
 		return text;
 	}
 
-	public InputStream getImage() {
-		return image;
+	public long getTmstmp() {
+		return tmstmp;
 	}
 
 	public double getLatitude() {
